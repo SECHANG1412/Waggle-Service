2a661df6576b2fb4d2b017cea24285f06a38de64
sechang <binaryblitz1412@gmail.com>
2026-07-21 08:25:35 +0900

    chore: k6 토픽 상세 대상 주소 변수화

--- k6/topic-detail-mid-load.js
+++ k6/topic-detail-mid-load.js
@@ -1,5 +1,7 @@
   import http from "k6/http";                                                                                             
   import { check, sleep } from "k6";                                                                                      
+
+const BASE_URL = __ENV.BASE_URL || "http://host.docker.internal:8000";
                                                                                                                           
   export const options = {                                                                                                
     vus: 10,
@@ -7,7 +9,7 @@
   };
                                                                                                                           
   export default function () {                                                                                            
-    const res = http.get("http://host.docker.internal:8000/topics/28");                                                   
+    const res = http.get(`${BASE_URL}/topics/28`);                                                   
     check(res, {                                                                                                          
       "status is 200": (r) => r.status === 200,                                                                           
     });                                                                                                                   
